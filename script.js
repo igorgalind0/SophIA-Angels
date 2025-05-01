65c2443979f3703ac012f71f647ad4b8e10a0b25
igorgalind0 <igorgalindo950@gmail.com>
2025-05-01 19:13:45 -0300

    atualizar animações

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 import initScrollSuave from './js/modules/scroll-suave.js';
 import initAnimacaoScroll from './js/modules/scroll-animacao.js';
 
+export default function CarrosselStack(){
+    window.onload = () => {
+        const list = document.getElementById('image-list');
+        const items = list.querySelectorAll('li'); // Seleciona todos os itens
+    
+        // Duplicando os itens da lista para garantir um ciclo contínuo
+        items.forEach(item => {
+            const clone = item.cloneNode(true); // Clona o item
+            list.appendChild(clone); // Adiciona a cópia ao final da lista
+        });
+    };
+}
+
 initScrollSuave();
 initAnimacaoScroll();
 

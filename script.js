6c6d7d7d440ff5536678d523997cf3a4f1006dd0
igorgalind0 <igorgalindo950@gmail.com>
2025-05-21 17:01:58 -0300

    página de time, animações, responsividade, menu mobile e atualizações

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
+const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const modalName = document.getElementById("modal-name");
+  const modalDesc = document.getElementById("modal-description");
+  const closeBtn = document.querySelector(".close");
+
+  const members = document.querySelectorAll(".time-list li");
+
+  members.forEach(member => {
+    member.addEventListener("click", () => {
+      const img = member.querySelector("img").src;
+      const name = member.querySelector("h1").innerText;
+      const desc = member.querySelector("p").innerText;
+
+      modalImg.src = img;
+      modalName.textContent = name;
+      modalDesc.textContent = desc;
+
+      modal.style.display = "block";
+    });
+  });
+
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
 import initScrollSuave from './js/modules/scroll-suave.js';
 import initAnimacaoScroll from './js/modules/scroll-animacao.js';
+import MenuMobile from './js/modules/menu-mobile.js';
+
+MenuMobile();
+initScrollSuave();
+initAnimacaoScroll();
+
+//Animação
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
 
 export default function CarrosselStack(){
     window.onload = () => {
@@ -12,12 +54,4 @@ export default function CarrosselStack(){
             list.appendChild(clone); // Adiciona a cópia ao final da lista
         });
     };
-}
-
-initScrollSuave();
-initAnimacaoScroll();
-
-//Animação
-if (window.SimpleAnime) {
-    new SimpleAnime();
 }
